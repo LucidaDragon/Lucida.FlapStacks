3cbf98af69f3ef6c2bfd282a771185abab36e3d9
Lucida Dragon <lucidadragon@gmail.com>
2021-03-15 15:48:18 -0400

    Updated BRA to JMP for 1.1 compliance.

--- Lucida.FlapStacks.Platform.URCL/Instructions/Jmp.cs
+++ Lucida.FlapStacks.Platform.URCL/Instructions/Jmp.cs
@@ -1,14 +1,14 @@
 ﻿namespace Lucida.FlapStacks.Platform.URCL.Instructions
 {
-	public class Bra : Instruction
+	public class Jmp : Instruction
 	{
-		public override string Keyword => "bra";
+		public override string Keyword => "jmp";
 
 		protected override int OperandCount => 1;
 
 		protected override Instruction CreateNew(string keyword)
 		{
-			return new Bra();
+			return new Jmp();
 		}
 
 		protected override void EmitCore(UrclConfig config, Emitter e)
