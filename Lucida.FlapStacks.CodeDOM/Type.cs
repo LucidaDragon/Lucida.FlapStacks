48e46f6922a6a806fe62b98635838259f9add53d
Lucida Dragon <lucidadragon@gmail.com>
2021-03-30 17:22:54 -0400

    Fixed constructor recursion.

--- Lucida.FlapStacks.CodeDOM/Type.cs
+++ Lucida.FlapStacks.CodeDOM/Type.cs
@@ -4,9 +4,11 @@ namespace Lucida.FlapStacks.CodeDOM
 {
 	public abstract class Type
 	{
+		public static readonly Type Void = new VoidType();
+
 		public string Name { get; set; }
 		public abstract ulong Size { get; }
-		public virtual Type PointerType { get; } = new VoidType();
+		public virtual Type PointerType => Void;
 		public virtual Type[] Fields { get; set; } = new Type[0];
 	}
 }
