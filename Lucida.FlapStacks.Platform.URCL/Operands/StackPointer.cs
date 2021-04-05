5cfdfeef7e9aac22ac9e3a8e859ce80e503d4c1e
Lucida Dragon <lucidadragon@gmail.com>
2021-04-05 13:11:47 -0400

    Added stack pointer.

---
+++ Lucida.FlapStacks.Platform.URCL/Operands/StackPointer.cs
@@ -0,0 +1,36 @@
+﻿namespace Lucida.FlapStacks.Platform.URCL.Operands
+{
+	public class StackPointer : Operand
+	{
+		public override Value Value => new Constant(0);
+
+		public override string GetString()
+		{
+			return "SP";
+		}
+
+		public override void Pop(Emitter e)
+		{
+			e.PopBasePointer();
+		}
+
+		public override void Push(Emitter e)
+		{
+			e.PushBasePointer();
+		}
+
+		public override bool TryParse(string str, out Operand operand)
+		{
+			if (str.ToUpper() == "SP")
+			{
+				operand = new StackPointer();
+				return true;
+			}
+			else
+			{
+				operand = null;
+				return false;
+			}
+		}
+	}
+}
